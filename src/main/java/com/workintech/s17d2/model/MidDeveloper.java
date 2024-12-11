dcbfa8f30323fd53246fc651b711752eba85597e
CihangirKELES <cihangirkeles@msn.com>
2024-12-11 13:29:29 +0300

    done

---
+++ src/main/java/com/workintech/s17d2/model/MidDeveloper.java
@@ -0,0 +1,7 @@
+package com.workintech.s17d2.model;
+
+public class MidDeveloper extends Developer {
+    public MidDeveloper(int id, String name, Double salary) {
+        super(Experience.MID, id, name, salary);
+    }
+}
